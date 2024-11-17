100a21062435233cb20e8cb3256ba8e6b607efbf
chiamaka28 <chiamakamaduike@gmail.com>
2024-11-17 18:30:51 +0100

    made some changes to my tailwind config file

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className='bg-white shadow-custom-dark  border-2 border-black flex flex-col justify-center items-center w-[90%] max-w-80 md:max-w-[21rem] py-5 px-4 rounded-xl transition duration-300 ease-in-out transform  hover:shadow-custom-hover'>
+    <article className='bg-white shadow-card  border-2 border-black flex flex-col justify-center items-center w-[90%] max-w-80 md:max-w-[21rem] py-5 px-4 rounded-xl transition duration-300 ease-in-out transform  hover:shadow-card-hover'>
       <div className=''>
         <img
           src='/illustration-article.svg'
@@ -11,25 +11,27 @@ const Card = () => {
         />
       </div>
       <div>
-        <div className='py-4 md:py-3  text-Gray950'>
+        <div className='py-4 md:py-3  text-gray-950'>
           <span className='bg-yellow rounded-sm text-[12px] font-bold px-3 py-1'>
             Learning
           </span>
-          <p className='text-[12px] py-3 text-Gray950'>Published 21 Dec 2023</p>
-          <h2 className='text-[20px] font-bold hover:text-yellow cursor-pointer pb-3 md:pb-2 text-Gray950'>
+          <p className='text-[12px] py-3 text-gray-950'>
+            Published 21 Dec 2023
+          </p>
+          <h2 className='text-[20px] font-bold hover:text-yellow cursor-pointer pb-3 md:pb-2 text-gray-950'>
             HTML & CSS foundations
           </h2>
-          <p className='text-[14px] text-Gray500'>
+          <p className='text-[14px] text-gray-500'>
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
         </div>
         <div className='flex items-center gap-3 font-bold'>
           <img src='/image-avatar.webp' className='w-8' alt='Avatar' />
-          <p className='text-Gray950'>Greg Hooper</p>
+          <p className='text-gray-950'>Greg Hooper</p>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        yellow: '#F4D04E',
-        Gray950: '#111111',
-        Gray500: '#6B6B6B',
-        White: '#FFFFFF',
-      },
-      fontFamily: { sans: ['Figtree', 'sans-serif'] },
-      boxShadow: {
-        'custom-dark': '0.5rem 0.5rem hsl(0, 0%, 7%)',
-        'custom-hover': '0.8rem 1rem hsl(0, 0%, 7%)',
-      },
+    extend: {},
+    colors: {
+      yellow: '#F4D04E',
+      'gray-950': '#111111',
+      'gray-500': '#6B6B6B',
+      white: '#FFFFFF',
+    },
+    fontFamily: { sans: ['Figtree', 'sans-serif'] },
+    boxShadow: {
+      card: '0.5rem 0.5rem hsl(0, 0%, 7%)',
+      'card-hover': '0.8rem 1rem hsl(0, 0%, 7%)',
     },
   },
   plugins: [],
